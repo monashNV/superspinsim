7f0287f481c54cf1aa5ab349ad5eea746394c703
Alexander Tritt Monash <alexander.tritt@monash.edu>
2024-11-18 16:44:39 +1100

    To Manjaro

--- activate_venv.ps1
+++
@@ -1,2 +0,0 @@
- C:\Users\User\.venv\spinsim-d\Scripts\Activate.ps1
- [Environment]::SetEnvironmentVariable('PYTHONPATH','C:\Users\User\.venv\spinsim-d\Scripts')
--- profile.ps1
+++ profile.ps1
@@ -1,2 +1,1 @@
-.\activate_venv.ps1
 nsys profile --trace cuda --gpu-metrics-devices=all --cuda-memory-usage true --force-overwrite true --output profile/profile python .
