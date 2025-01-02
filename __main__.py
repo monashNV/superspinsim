369c7b406c8e4dac8efab917453719954812a252
alexander-tritt-monash <alexander.tritt@monash.edu>
2025-01-02 19:37:37 +1100

    Tried to scaffold

--- __main__.py
+++ __main__.py
@@ -1,114 +1,52 @@
+# https://doi.org/10.1016/j.apnum.2005.11.004
+
+import math
 import numpy as np
-from numpy import random as npr
-from numba import cuda as nc
-import time as tm
 
 
-@nc.jit(device=True)
-def residual_square(matrix_input, matrix_output):
-    if not (nc.threadIdx.x < 9 and nc.threadIdx.y < 9):
-        return
-    matrix_output[nc.blockIdx.x, nc.threadIdx.y, nc.threadIdx.x] = \
-        2*matrix_input[nc.blockIdx.x, nc.threadIdx.y, nc.threadIdx.x]
-    for interior_index in range(9):
-        matrix_output[nc.blockIdx.x, nc.threadIdx.y, nc.threadIdx.x] = \
-            nc.fma(
-                matrix_input[nc.blockIdx.x, nc.threadIdx.y, interior_index],
-                matrix_input[nc.blockIdx.x, interior_index, nc.threadIdx.x],
-                matrix_output[nc.blockIdx.x, nc.threadIdx.y, nc.threadIdx.x]
-            )
+use residual = True
 
 
-@nc.jit
-def multi_residual_square(matrix_input, matrix_output):
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
-    residual_square(matrix_input, matrix_output)
-    nc.syncthreads()
-    residual_square(matrix_output, matrix_input)
-    nc.syncthreads()
+weights_cf_4_2 = np.array([
+        [(3 - 2*math.sqrt(3))/12, (3 + 2*math.sqrt(3))/12],
+        [(3 + 2*math.sqrt(3))/12, (3 - 2*math.sqrt(3))/12]
+])
+
+nodes_cf_4_2 = np.array([(3 - math.sqrt(3)/6, (3 - math.sqrt(3)/6])
+
+def _calculate_time(time, time_sample):
+    pass
+
+
+def _sample(time, coefficient):
+    pass
+
+
+def _calculate_differential(time_step, generator, coefficient, differential):
+    pass
+
+
+def _massage_differential(differential):
+    pass
+
+
+if use_residual:
+    def _square(operator, operator_out, operator_index, column_index, row_index):
+        operator_out[operator_index, column_index, row_index] = \
+            2*operator[operator_index, column_index, row_index]
+
+
+def _repeated_square(operator):
+    pass
+
+
+def _partial_combine(unitary, new_unitary):
+    pass
+
+
+def _telescope_combine(unitary):
+    pass
 
 
 if __name__ == "__main__":
-    rng = npr.default_rng(1234567890)
-    matrix = rng.standard_normal((1000000, 9, 9), dtype=np.float64)/pow(2, 32)
-
-    wall_time_start = tm.time()
-
-    matrix_device = nc.to_device(matrix)
-    output_device = nc.device_array_like(matrix_device)
-
-    wall_time_loaded = tm.time()
-
-    multi_residual_square[(1000000, 1), (9, 9)](matrix_device, output_device)
-
-    wall_time_computed = tm.time()
-
-    output = output_device.copy_to_host()
-
-    wall_time_copied = tm.time()
-
-    print(wall_time_loaded - wall_time_start)
-    print(wall_time_computed - wall_time_loaded)
-    print(wall_time_copied - wall_time_computed)
-    print(wall_time_copied - wall_time_start)
-
-    print(output)
+    pass
