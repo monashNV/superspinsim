83993790fe3bf84b91f1987a1a6e000e2cd46f3b
Alexander Tritt Monash <alexander.tritt@monash.edu>
2025-01-23 17:11:23 +1100

    debug

--- read_profile.py
+++ read_profile.py
@@ -4,44 +4,86 @@ import h5py
 
 from matplotlib import pyplot as plt
 from cmcrameri import cm
-    
+
 datatype = np.float64
 
-if __name__ == "__main__":
+
+def read_h5():
     with h5py.File("profile/profile.h5", "r") as h5_file:
-        name_ids = []
-        start_times = []
-        end_times = []
+        kernel_name_ids = []
+        kernel_start_times = []
+        kernel_end_times = []
         kernel_trace_h5 = h5_file["CUPTI_ACTIVITY_KIND_KERNEL"]
         for entry_h5 in kernel_trace_h5:
-            start_times.append(entry_h5[0])
-            end_times.append(entry_h5[1])
-            name_ids.append(entry_h5[9])
+            kernel_start_times.append(entry_h5[0])
+            kernel_end_times.append(entry_h5[1])
+            kernel_name_ids.append(entry_h5[9])
 
-        names = []
+        kernel_names = []
         id_map_h5 = h5_file["StringIds"]
-        for name_id in name_ids:
+        for name_id in kernel_name_ids:
             name = id_map_h5[name_id][1].decode("utf-8")
-            names.append(name)
+            kernel_names.append(name)
 
+        memory_name_ids = []
+        memory_start_times = []
+        memory_end_times = []
+        memory_trace_h5 = h5_file["CUPTI_ACTIVITY_KIND_MEMCPY"]
+        for entry_h5 in memory_trace_h5:
+            memory_start_times.append(entry_h5[0])
+            memory_end_times.append(entry_h5[1])
+            memory_name_ids.append(entry_h5[9])
+
+        memory_names = []
+        id_map_h5 = h5_file["ENUM_CUDA_MEMCPY_OPER"]
+        for name_id in memory_name_ids:
+            name = id_map_h5[name_id][2].decode("utf-8")
+            memory_names.append(name)
+
+    names = kernel_names + memory_names
+    start_times = kernel_start_times + memory_start_times
+    end_times = kernel_end_times + memory_end_times
+
+    # names = kernel_names
+    # start_times = kernel_start_times
+    # end_times = kernel_end_times
+
+    start_times = np.array(start_times)
+    end_times = np.array(end_times)
+    order = np.argsort(start_times)
+    start_times = start_times[order]
+    end_times = end_times[order]
+    names = [names[order_index] for order_index in order]
+    print(order)
+
+    return names, start_times, end_times
+
+
+def calculate_durations(start_times, end_times):
     durations = []
     for start_time, end_time in zip(start_times, end_times):
         durations.append(end_time - start_time)
+    return durations
 
+
+def clean_names(names):
     for name_index, name in enumerate(names):
         names[name_index] = name.partition("kernel")[0].strip("_")
 
+
+def split_into_trials(names, start_times, end_times):
     trials = []
-    trial  = None
+    trial = {"names": [], "durations": []}
     for name, duration in zip(names, durations):
-        if name == names[0]:
-            if trial is not None:
-                trials.append(trial)
-            trial = {"names": [], "durations": []}
         trial["names"].append(name)
         trial["durations"].append(duration)
-    trials.append(trial)
+        if name == "Host-To-Device":
+            trials.append(trial)
+            trial = {"names": [], "durations": []}
+    return trials
 
+
+def accumulate(trials, verbose=False):
     for trial_index, trial in enumerate(trials):
         durations_total = {}
         times_executed = {}
@@ -61,15 +103,24 @@ if __name__ == "__main__":
         trial["duration_total"] = duration_total
         trial["times_executed"] = times_executed
 
-        print(f"TRIAL {trial_index}")
-        print(f"| {'Kernel':32s} | {'Times executed':16} | {'Total duration (ns)':24} |")
-        print(f"|-{'-'*32}-|-{'-'*16}-|-{'-'*24}-|")
-        for name, duration in durations_total.items():
-            print(f"| {name:32s} | {times_executed[name]:16d} | {duration:24d} |")
+        if verbose:
+            print(f"TRIAL {trial_index}")
+            print(
+                f"| {'Kernel':32s} | {'Times executed':16} | "
+                f"{'Total duration (ns)':24} |"
+            )
+            print(f"|-{'-'*32}-|-{'-'*16}-|-{'-'*24}-|")
+            for name, duration in durations_total.items():
+                print(
+                    f"| {name:32s} | {times_executed[name]:16d} | "
+                    f"{duration:24d} |"
+                )
 
-        print(f"| {'TOTAL':32s} | {'':16s} | {duration_total:24d} |")
-        print("")
+            print(f"| {'TOTAL':32s} | {'':16s} | {duration_total:24d} |")
+            print("")
 
+
+def generate_slices(trials):
     durations_total = []
     durations_slice = {}
     for trial in trials:
@@ -79,36 +130,67 @@ if __name__ == "__main__":
                 durations_slice[name].append(duration)
             else:
                 durations_slice[name] = [duration]
-    print(durations_total)
 
     durations_total = np.array(durations_total, dtype=datatype)
     for name, duration in durations_slice.items():
         durations_slice[name] = np.array(duration, dtype=datatype)
 
+    return durations_total, durations_slice
+
+
+def combine_related(durations_slice):
     durations_important = {}
-    durations_important["Quadrature sample"] = durations_slice["calculate_time_quadrature"] \
+    durations_important["Quadrature sample"] = \
+        durations_slice["calculate_time_quadrature"] \
         + durations_slice["sample"] \
         + durations_slice["combine_coefficients"] \
         + durations_slice["quadrature_combine"]
-    durations_important["Lie algebra to matrix"] = durations_slice["calculate_differential"] \
+    durations_important["Lie algebra to matrix"] = \
+        durations_slice["calculate_differential"] \
         + durations_slice["scale_differential_basic"]
-    durations_important["Cayley transform"] = durations_slice["calculate_cayley"]
-    durations_important["Squaring"] = durations_slice["repeated_quartic_superoperator"]
-    durations_important["Accumulate time evolution"] = durations_slice["id_superoperator"] \
+    durations_important["Cayley transform"] = \
+        durations_slice["calculate_cayley"]
+    durations_important["Squaring"] = \
+        durations_slice["repeated_quartic_superoperator"]
+    durations_important["Accumulate time evolution"] = \
+        durations_slice["id_superoperator"] \
         + durations_slice["basic_combine"]
-    durations_important["Apply to state"] = durations_slice["calculate_time_basic"] \
+    durations_important["Apply to state"] = \
+        durations_slice["calculate_time_basic"] \
         + durations_slice["apply_time_evolution"]
 
-    trial = np.arange(durations_total.size)
+    return durations_important
+
+
+def plot(durations_total, durations_slice):
+    trial = np.arange(durations_total.shape[0])
 
     plt.figure()
-    plt.plot(trial, np.log10(durations_total) - 9, ".-k", label="Total")
-    number_of_plots = len(durations_important)
-    for plot_index, (name, duration) in enumerate(durations_important.items()):
-        plt.plot(trial, np.log10(duration) - 9, ".--", color=cm.hawaii(plot_index/number_of_plots), label=name)
+    plt.plot(trial, durations_total*1e-9, ".-k", label="Total")
+    number_of_plots = len(durations_slice)
+    for plot_index, (name, duration) in enumerate(durations_slice.items()):
+        plt.plot(
+            trial,
+            duration*1e-9,
+            ".--",
+            color=cm.hawaii(plot_index/number_of_plots),
+            label=name
+        )
     plt.xlabel("Trial index")
-    plt.ylabel("lg Time (s)")
+    plt.yscale("log")
+    plt.ylabel("Time (s)")
     plt.legend()
     plt.draw()
+
+
+if __name__ == "__main__":
+    names, start_times, end_times = read_h5()
+    durations = calculate_durations(start_times, end_times)
+    clean_names(names)
+    trials = split_into_trials(names, start_times, end_times)
+    accumulate(trials, verbose=True)
+    durations_total, durations_slice = generate_slices(trials)
+    durations_important = combine_related(durations_slice)
+    plot(durations_total, durations_slice)
+    plot(durations_total, durations_important)
     plt.show()
-    print(trial, duration_total)
