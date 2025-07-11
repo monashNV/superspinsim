aad3acb7cdcb7386099799e0fed99b483cb3c13c
Alexander Tritt Monash <alexander.tritt@monash.edu>
2025-07-11 17:51:50 +1000

    Underwhelming

--- __main__.py
+++ __main__.py
@@ -16,19 +16,19 @@ if __name__ == "__main__":
     #         ["s", None, "s", None])(main)
     #     main()
 
-    # from comparisons.comp_superspinsim import main
-    # with Pogger("superspinsim-comparisons") as logger:
-    #     logger.set_context("superspinsim")
-    #     main = logger.record(
-    #         ["fine_division", "density", "wall_duration", "error"],
-    #         [None, None, "s", None])(main)
-    #     main()
-
-    from comparisons.compilation import main
+    from comparisons.comp_superspinsim import main
     with Pogger("superspinsim-comparisons") as logger:
-        logger.set_context("compilation")
-        main = logger.record(["protocols", "pca_data"])(main)
+        logger.set_context("superspinsim")
+        main = logger.record(
+            ["fine_division", "density", "wall_duration", "error"],
+            [None, None, "s", None])(main)
         main()
+
+    # from comparisons.compilation import main
+    # with Pogger("superspinsim-comparisons") as logger:
+    #     logger.set_context("compilation")
+    #     main = logger.record(["protocols", "pca_data"])(main)
+    #     main()
 
     # from superspinsim.params import write_values
     # write_values()
