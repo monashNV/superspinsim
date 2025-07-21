057fdf2144c1f1a557f0e91a56c305227cf27263
Alexander Tritt Monash <alexander.tritt@monash.edu>
2025-07-21 19:09:08 +1000

    Got lindblad in qtjl

--- comparisons/comp_quantum_toolbox.jl
+++ comparisons/comp_quantum_toolbox.jl
@@ -1,19 +1,104 @@
 using HDF5
 using QuantumToolbox
 
+function generate_coefficients_contrast()
+	duration_excitation = 3e-6
+	duration_relax_wait = 250e-9
+	duration_mw = 1/(2*10e6)
+	frequency_mw = 2.87e9 - 280e6
+	amplitude_mw = sqrt(1/2)*2*10e6/28e9
+	excitation_amplitude = 0.1
+
+	time_thermal_start = 0
+	time_thermal_end = time_thermal_start + duration_excitation
+	time_zero_start = duration_excitation + duration_relax_wait
+	time_zero_end = time_zero_start + duration_excitation
+	time_one_start = 2*duration_excitation + 2*duration_relax_wait + duration_mw
+	time_one_end = time_one_start + duration_excitation
+	time_end = 10e-6
+	tau = 2*pi
+
+	function coefficient_x(p, t)
+		if t < time_one_start - duration_mw
+		    return 0
+		end
+		if t < time_one_start
+			return amplitude_mw*sin(tau*frequency_mw*t)
+		end
+		return 0
+	end
+
+	coefficient_y(p, t) = 0
+	coefficient_z(p, t) = 0
+
+	function coefficient_l(p, t)
+		if t < time_thermal_end
+			return sqrt(excitation_amplitude)
+		end
+		if t < time_zero_start
+			return 0
+		end
+		if t < time_zero_end
+			return sqrt(excitation_amplitude)
+		end
+		if t < time_one_start
+			return 0
+		end
+		return sqrt(excitation_amplitude)
+        end
+
+	return coefficient_x, coefficient_y, coefficient_z, coefficient_l
+end
+
 # Read from python
 h5_file = h5open("to_julia.h5", "r")
 time_step = attrs(h5_file)["time_step"]
 time_end = attrs(h5_file)["time_end"]
+density_operator_initial = Qobj(read(h5_file["density_operator_initial"]))
+generator_x = Qobj(read(h5_file["generators_coherent/1"]))
+generator_y = Qobj(read(h5_file["generators_coherent/2"]))
+generator_z = Qobj(read(h5_file["generators_coherent/3"]))
+generator_0 = Qobj(read(h5_file["generators_coherent/4"]))
+
+index = 1
+jumps_static = []
+while haskey(h5_file, "generators_jump_static/"*string(index))
+	push!(jumps_static, Qobj(read(h5_file["generators_jump_static/"*string(index)])))
+	global index += 1
+end
+
+index = 1
+jumps_dynamic = []
+while haskey(h5_file, "generators_jump_dynamic/"*string(index))
+	push!(jumps_dynamic, Qobj(read(h5_file["generators_jump_dynamic/"*string(index)])))
+	global index += 1
+end
+
 close(h5_file)
 
-print(time_step)
-print(time_end)
+# Convert to QuantumToolboxJL language
+density_operator_initial = Qobj(density_operator_initial)
+coefficient_x, coefficient_y, coefficient_z, coefficient_l = generate_coefficients_contrast()
+hamiltonian = QobjEvo((
+	generator_0,
+	(generator_x, coefficient_x),
+	(generator_y, coefficient_y),
+	(generator_z, coefficient_z)
+))
 
-times = 0:time_step:time_end
-print(times)
+jumps_dynamic_true = []
+for jump_dynamic in jumps_dynamic
+	push!(jumps_dynamic_true, QobjEvo(jump_dynamic, coefficient_l))
+end
+jumps = vcat(jumps_static, jumps_dynamic_true)
+
+time = 0:time_step:time_end
+
+result = mesolve(hamiltonian, density_operator_initial, time, jumps)
+density = result.states
 
 # Send to python
 h5_file = h5open("from_julia.h5", "w")
-h5_file["times"] = collect(times)
+h5_file["time"] = collect(time)
+h5_file["density"] = stack(get_data.(density))
 close(h5_file)
--- comparisons/comp_quantum_toolbox.py
+++ comparisons/comp_quantum_toolbox.py
@@ -2,6 +2,8 @@ import os
 import h5py
 import numpy as np
 
+from matplotlib import pylab as plt
+
 from comparisons.lindbladians import contrast
 
 
@@ -15,10 +17,34 @@ def main(lindbladian="contrast"):
     with h5py.File("to_julia.h5", "w") as h5_file:
         h5_file.attrs["time_step"] = time_step
         h5_file.attrs["time_end"] = time_end
+        h5_file["density_operator_initial"] = density_initial
+        group_coherent = h5_file.create_group("generators_coherent")
+        for index, generator_coherent in enumerate(generators_coherent):
+            group_coherent[str(index + 1)] = generator_coherent
+        group_jump_static = h5_file.create_group("generators_jump_static")
+        for index, generator_jump in enumerate(generators_jump[0]):
+            group_jump_static[str(index + 1)] = generator_jump
+        group_jump_dynamic = h5_file.create_group("generators_jump_dynamic")
+        for index, generator_jump in enumerate(generators_jump[1]):
+            group_jump_dynamic[str(index + 1)] = generator_jump
 
     os.system("julia --project=julia_env comparisons/comp_quantum_toolbox.jl")
 
     with h5py.File("from_julia.h5", "r") as h5_file:
-        print(np.asarray(h5_file["times"].shape))
+        time = np.asarray(h5_file["time"])
+        density = np.asarray(h5_file["density"])
     os.remove("to_julia.h5")
     os.remove("from_julia.h5")
+
+    print(density.shape)
+
+    fluorescence = \
+        np.real(density[:, 3, 3] + density[:, 4, 4] + density[:, 5, 5])
+    fluorescence /= np.max(fluorescence)
+
+    plt.figure(label="fluorescence")
+    plt.plot(time/1e-6, fluorescence/0.01, "k-")
+    plt.xlabel("Time (us)")
+    plt.ylabel("Fluorescence (%)")
+    plt.gca().spines[["top", "right"]].set_visible(False)
+    plt.show()
