092170c89c0e5f4da229c86eee0f765f29ac9320
Alexander Tritt Monash <alexander.tritt@monash.edu>
2025-07-21 17:12:49 +1000

    To and from python

--- comparisons/comp_quantum_toolbox.jl
+++ comparisons/comp_quantum_toolbox.jl
@@ -1,10 +1,19 @@
 using HDF5
 using QuantumToolbox
 
-println("Hello world, this is me")
+# Read from python
+h5_file = h5open("to_julia.h5", "r")
+time_step = attrs(h5_file)["time_step"]
+time_end = attrs(h5_file)["time_end"]
+close(h5_file)
 
-h5_file = h5open("julia_save.h5", "w")
+print(time_step)
+print(time_end)
 
-attributes(h5_file)["hello world"] = "This is me"
+times = 0:time_step:time_end
+print(times)
 
+# Send to python
+h5_file = h5open("from_julia.h5", "w")
+h5_file["times"] = collect(times)
 close(h5_file)
--- comparisons/comp_quantum_toolbox.py
+++ comparisons/comp_quantum_toolbox.py
@@ -1,9 +1,24 @@
 import os
 import h5py
+import numpy as np
+
+from comparisons.lindbladians import contrast
 
 
-def main():
+def main(lindbladian="contrast"):
+    if lindbladian == "contrast":
+        generate_return, generate_return_comparison = contrast()
+
+    coefficients, generators_coherent, generators_jump, density_initial, \
+        time_step, time_end = generate_return_comparison
+
+    with h5py.File("to_julia.h5", "w") as h5_file:
+        h5_file.attrs["time_step"] = time_step
+        h5_file.attrs["time_end"] = time_end
+
     os.system("julia --project=julia_env comparisons/comp_quantum_toolbox.jl")
-    with h5py.File("julia_save.h5", "r") as h5_file:
-        print(h5_file.attrs["hello world"].decode("utf-8"))
-    os.remove("julia_save.h5")
+
+    with h5py.File("from_julia.h5", "r") as h5_file:
+        print(np.asarray(h5_file["times"].shape))
+    os.remove("to_julia.h5")
+    os.remove("from_julia.h5")
