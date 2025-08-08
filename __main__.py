07c833720379b72b9e8bec287ecd35b22cd0463a
Alexander Tritt Monash <alexander.tritt@monash.edu>
2025-08-08 17:55:07 +1000

    Better odmr plot

--- __main__.py
+++ __main__.py
@@ -21,22 +21,22 @@ if __name__ == "__main__":
     # from comparisons.comp_quantum_toolbox import main
     # main()
 
-    # from comparisons.comp_superspinsim import main
-    # main(
-    #     lindbladian="odmr",
-    #     use_rotating=True,
-    #     number_of_exponentials=5
-    # )
+    from comparisons.comp_superspinsim import main
+    main(
+        lindbladian="odmr",
+        use_rotating=True,
+        number_of_exponentials=5
+    )
 
-    from matplotlib import pyplot as plt
-    from pogger import Pogger
+    # from matplotlib import pyplot as plt
+    # from pogger import Pogger
 
-    from comparisons.compilation import main
-    with Pogger("superspinsim-comparisons") as logger:
-        logger.set_context("compilation")
-        main = logger.record(["protocols", "pca_data"])(main)
-        main()
-    plt.show()
+    # from comparisons.compilation import main
+    # with Pogger("superspinsim-comparisons") as logger:
+    #     logger.set_context("compilation")
+    #     main = logger.record(["protocols", "pca_data"])(main)
+    #     main()
+    # plt.show()
 
     # from superspinsim.params import write_values
     # write_values()
