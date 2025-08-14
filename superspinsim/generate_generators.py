145a522fdbaec51666329edcd4ff2cf542a8d416
Alexander Tritt Monash <alexander.tritt@monash.edu>
2025-08-14 18:06:44 +1000

    Added superoperators

--- superspinsim/generate_generators.py
+++ superspinsim/generate_generators.py
@@ -1,5 +1,4 @@
 import numpy as np
-# import scipy.linalg as spl
 import math
 import copy
 
--- superspinsim/sugar.py
+++ superspinsim/sugar.py
@@ -1,27 +1,67 @@
 import numpy as np
 
 from superspinsim.generate_simulator import generate_simulator
+from superspinsim.generate_generators import \
+    _generate_valid_indices, _generate_superoperators
 
-
-def mesolve(H, rho0: np.ndarray, ti: float, tf: float, dt: float, c_ops=None):
+def mesolve(
+        H, rho0: np.ndarray, ti: float, tf: float, dt: float, c_ops=None,
+        allowed: np.ndarray = None, use_rotating: bool = True):
     """
         A wrapper for superspinsim to provide a similar syntax to other
         simulators.
     """
 
     # Sort hamiltonians
-    ham_quiescent, ham_time_dependent, ham_coeficients = \
+    ham_quiescent, ham_time_dependent, ham_coefficients = \
         _sort_operators(H, "H")
+    ham_quiescent = [sum(ham_quiescent)]
     # Sort collapse operators
-    ham_quiescent, ham_time_dependent, ham_coeficients = \
+    jump_quiescent, jump_time_dependent, jump_coefficients = \
         _sort_operators(c_ops, "c_ops")
+
+    operator_dict = {}
+    for gen_index, gen in enumerate(ham_quiescent):
+        operator_dict[f"Hq{gen_index}"] = gen
+    for gen_index, gen in enumerate(ham_time_dependent):
+        operator_dict[f"Ht{gen_index}"] = gen
+    for gen_index, gen in enumerate(jump_quiescent):
+        operator_dict[f"Lq{gen_index}"] = gen
+    for gen_index, gen in enumerate(jump_time_dependent):
+        operator_dict[f"Lt{gen_index}"] = gen
+
+    new_operator_dict = {}
+    for label, operator in operator_dict.items():
+        print(label, operator)
+        new_operator = np.empty(
+            (operator.shape[0], operator.shape[1], 2), np.float64)
+        new_operator[:, :, 0] = np.real(operator)
+        new_operator[:, :, 1] = np.imag(operator)
+        new_operator_dict[label] = new_operator
+    operator_dict = new_operator_dict
+
+    if allowed is None:
+        if len(ham_quiescent) > 0:
+            allowed = np.ones_like(ham_quiescent[0])
+        elif len(ham_time_dependent) > 0:
+            allowed = np.ones_like(ham_time_dependent[0])
+        elif len(jump_quiescent) > 0:
+            allowed = np.ones_like(jump_quiescent[0])
+        elif len(jump_time_dependent) > 0:
+            allowed = np.ones_like(jump_time_dependent[0])
+        else:
+            raise TypeError("No operators specified.")
+    valid_indices = _generate_valid_indices(allowed)
+
+    superoperators = _generate_superoperators(operator_dict, valid_indices)
+    print(superoperators)
 
 
 def _sort_operators(operators, operator_label: str):
     type_error_message = \
         f"{operator_label} must be numpy.ndarray, an [np.ndarray, function]" \
         + "list, or list of either."
-    quiescent = None
+    quiescent = []
     time_dependent = []
     coefficients = []
     return_value = [quiescent, time_dependent, coefficients]
@@ -33,6 +73,7 @@ def _sort_operators(operators, operator_label: str):
     # Is there just one operator?
     if isinstance(operators, np.ndarray):
         quiescent.append(operators)
+        return return_value
     elif type(operators) is not list:
         raise TypeError(type_error_message)
 
@@ -50,10 +91,7 @@ def _sort_operators(operators, operator_label: str):
     for operator in operators:
         # Time-independent case
         if isinstance(operator, np.ndarray):
-            if quiescent is None:
-                quiescent = operator.copy()
-            else:
-                quiescent += operator
+            quiescent.append(operator)
             continue
 
         # Time-dependent case
